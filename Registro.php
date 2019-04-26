61fb8155d380867a9324b725ee653ecad6cc1320
UccTube <ucctubevideo@gmail.com>
2019-04-25 23:39:45 -0500

    headers

--- Registro.php
+++ Registro.php
@@ -1,7 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+if($_SERVER['HTTP_ORIGIN'] == "https://ucctubemedia.netlify.com") {
+  header("Access-Control-Allow-Origin: https://ucctubemedia.netlify.com");
+  echo 'Server connected';
+}
 
 include("api.php");
 
