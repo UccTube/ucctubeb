42db278c5454579bb04e5067eac90567a0ce0556
UccTube <ucctubevideo@gmail.com>
2019-04-27 15:38:31 -0500

    headers acess

--- web/index.php
+++ web/index.php
@@ -1,5 +1,10 @@
 <?php
- header("Access-Control-Allow-Origin: *");
+ $origin = $_SERVER['HTTP_ORIGIN'];
+ $allowed_domains = [
+     'https://ucctubemedia.netlify.com'
+ ];
+ if (in_array($origin, $allowed_domains)) {
+     header('Access-Control-Allow-Origin: ' . $origin);
+ }
  echo "funciono!!";
-
 ?>
