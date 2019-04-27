e1195412c270e78d89eb4ce5bc69a05c3fd84c87
UccTube <ucctubevideo@gmail.com>
2019-04-27 16:39:14 -0500

    headers added

--- web/index.php
+++ web/index.php
@@ -1,7 +1,4 @@
 <?php
     header('Access-Control-Allow-Origin: https://ucctubemedia.netlify.com');
-    header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-    header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-    header("Access-Control-Allow-Credentials", true);
     echo "funciono!!";
 ?>
