79f28b40d999e717fe245355cf97391a24babe7d
UccTube <ucctubevideo@gmail.com>
2019-04-27 16:01:36 -0500

    headers removed

--- web/index.php
+++ web/index.php
@@ -1,3 +1,7 @@
 <?php
+    header('Access-Control-Allow-Origin: https://ucctubemedia.netlify.com');
+    header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+    header("Access-Control-Allow-Credentials", true);
     echo "funciono!!";
 ?>
