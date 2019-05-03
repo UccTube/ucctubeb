0c770fa5ba2f150bb8947401ccefde7bd0ebec6d
UccTube <ucctubevideo@gmail.com>
2019-05-02 19:24:00 -0500

    Validar un usuario

--- web/metodosDB.php
+++ web/metodosDB.php
@@ -42,7 +42,7 @@
     function buscarInformacion($informacion, $opcion){
 
         $metodo = explode($opcion, " ");
-        echo $metodo[0]." -- ".$metodo[1];
+        echo $opcion." -- ".$metodo[0]." -- ".$metodo[1];
 
         switch ($metodo[0]){
             case "Usuario":
