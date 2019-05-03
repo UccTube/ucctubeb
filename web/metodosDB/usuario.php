7ee1cb538efa5d3467fae015132f83c2244cc8e7
UccTube <ucctubevideo@gmail.com>
2019-05-02 19:09:09 -0500

    Validar un usuario

--- web/metodosDB/usuario.php
+++ web/metodosDB/usuario.php
@@ -4,17 +4,17 @@
     * Buscar usuarios
     */
 
-    $buscar_usuario = "SELECT correo FROM usuario WHERE correo ='".$informacion[2]."'";
-
     function buscarUsuario($informacion){
         $db = Db_conexion::getInstance();
+        $buscar_usuario = "SELECT correo FROM usuario WHERE correo ='".$informacion[2]."'";
         $result = pg_query($db, $buscar_usuario);
     }
 
     function buscarUsuarioRegistrado($informacion){
         $db = Db_conexion::getInstance();
+        $buscar_usuario = "SELECT correo FROM usuario WHERE correo ='".$informacion[2]."'";
         $result = pg_query($db, $buscar_usuario);
-        echo "ENTRO  ".pg_num_rows($result);
+        echo pg_num_rows($result);
     }
 
 ?>        
