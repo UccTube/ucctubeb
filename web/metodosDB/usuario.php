f81e53965fc6e52659393d531e8afdc1192e3342
click-academy <click-academy@gmail.com>
2019-05-05 18:09:09 -0500

    promise fixing

--- web/metodosDB/usuario.php
+++ web/metodosDB/usuario.php
@@ -3,17 +3,15 @@
     /**
     * Buscar usuarios
     */
-    $buscar_usuario = "SELECT correo FROM usuario WHERE correo ='".$informacion[2]."'";
     function buscarUsuario($informacion){
         $db = Db_conexion::getInstance();
         $result = pg_query($db, $buscar_usuario);
     }
 
     function buscarUsuarioRegistrado($informacion){
+        $buscar_usuario = "SELECT correo FROM usuario WHERE correo ='".$informacion[2]."'";
         $db = Db_conexion::getInstance();
         $result = pg_query($db, $buscar_usuario);
-        echo 'u = '.$db.", ".$buscar_usuario;
-        echo ', r = '.$result;
         echo pg_num_rows($result);
     }
 
