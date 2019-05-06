5d0d0e7ca8364dd42e71b47ea1b6d4561f6116bc
click-academy <click-academy@gmail.com>
2019-05-05 20:10:45 -0500

    guardar usuario bd

--- web/Registro.php
+++ web/Registro.php
@@ -14,6 +14,7 @@
     if(
       isset($_POST["guardarDatos"])
     ){
+      echo "ENTRO";
       require 'metodosDB.php';      
       dirigirInformacion($_POST["guardarDatos"], 'Insertar', 'Usuario');
     }
